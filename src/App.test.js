65b60dc9051df4823af4585eed17e2cbadf04dc6
Mel Incognito <melxincognito@gmail.com>
2022-01-15 22:14:21 -0700

    testing async events

--- src/App.test.js
+++ src/App.test.js
@@ -3,9 +3,9 @@ import { render, screen } from "@testing-library/react";
 import App from "./App";
 
 describe("App", () => {
-  test("renders App component", () => {
+  test("renders App component", async () => {
     render(<App />);
-
-    expect(screen.getByText("Search:")).toBeInTheDocument();
+    expect(screen.queryByText(/Signed in as/)).toBeNull();
+    expect(await screen.findByText(/Signed in as/)).toBeInTheDocument();
   });
 });
