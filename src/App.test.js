530a40168dddf9063efaff304dd2a48b77f14c25
Mel Incognito <melxincognito@gmail.com>
2022-01-15 23:14:52 -0700

    added 2 debugs to see before and after async

--- src/App.test.js
+++ src/App.test.js
@@ -6,6 +6,8 @@ describe("App", () => {
   test("renders App component", async () => {
     render(<App />);
     expect(screen.queryByText(/Signed in as/)).toBeNull();
+    screen.debug();
     expect(await screen.findByText(/Signed in as/)).toBeInTheDocument();
+    screen.debug();
   });
 });
