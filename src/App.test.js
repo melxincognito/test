d2358f8b4fe8bd0705b34f9dfece17cc6ccfd2bb
Mel Incognito <melxincognito@gmail.com>
2022-01-16 01:31:09 -0700

    using userEvent

--- src/App.test.js
+++ src/App.test.js
@@ -1,34 +1,22 @@
 import React from "react";
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import App from "./App";
 
-// normal testing w fire event
-describe("App", () => {
-  test("renders App component", () => {
-    render(<App />);
-    screen.debug();
-    fireEvent.change(screen.getByRole("textbox"), {
-      target: { value: "Mel Incognito" },
-    });
+// normal testing with userEvent async events
 
-    screen.debug();
-  });
-});
-
-// setting with an async event
-// dont forget to add await
 describe("App", () => {
-  test("renders the App component", async () => {
+  test("renders App component", async () => {
     render(<App />);
+
     await screen.findByText(/Signed in as/);
-    expect(screen.queryByText(/Searches for Mel Incognito/)).toBeNull(); //telling your test nothing should be in the document
 
-    fireEvent.change(screen.getByRole("textbox"), {
-      target: { value: "Mel Incognito" },
-    });
+    expect(screen.queryByText(/Searches for Mel Incognito/)).toBeNull();
 
-    expect(
-      screen.queryByText(/Searches for Mel Incognito/)
-    ).toBeInTheDocument(); //searching for this after it's been rendered
+    await userEvent.type(screen.getByRole("textbox"), "Mel Incognito");
+    expect(screen.getByText(/Searches for Mel Incognito/)).toBeInTheDocument();
   });
 });
+
+// use userEvent over fireEvent whenever you can
+// userEvent doesn't have all the features of fireEvent atm
